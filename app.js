7024d2736ab9af6b87a3a07307b5dc0f8065f6c7
rahull0328 <rmmehta2004@gmail.com>
2025-08-21 21:56:29 +0530

    db config complete

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 import express from "express"
+import { nanoid } from "nanoid"
+import dotenv from "dotenv"
+import connectDB from "./config/mongoose.config.js"
+
+dotenv.config({})
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.post("/api/create", (req, res) => {
+    const {url} = req.body
+    res.send(nanoid(7))
+})
+
 app.listen(5000, () => {
+    connectDB()
     console.log("Server running on port 5000")
 })
