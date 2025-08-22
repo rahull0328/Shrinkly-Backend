3097994306f183e509ace8139ded44e958e00870
rahull0328 <rmmehta2004@gmail.com>
2025-08-22 21:04:11 +0530

    categorized api for creating short url

---
+++ controllers/shortUrl.controller.js
@@ -0,0 +1,8 @@
+import { createShortUrlService } from "../services/shortUrl.service.js"
+
+
+export const createShortUrl = async (req, res) => {
+    const {url} = req.body
+    const shortUrl = await createShortUrlService(url)
+    res.send(process.env.APP_URL + "/" + shortUrl)
+}
