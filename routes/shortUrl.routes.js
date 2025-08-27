7f34b1c4d66f4ac0534011565d6687df59c35a54
rahull0328 <rmmehta2004@gmail.com>
2025-08-27 17:07:15 +0530

    major advancements

--- routes/shortUrl.routes.js
+++ routes/shortUrl.routes.js
@@ -1,9 +1,8 @@
 import express from "express"
-import { createShortUrl, redirectFromShortUrl } from "../controllers/shortUrl.controller.js"
+import { createShortUrl } from "../controllers/shortUrl.controller.js"
 
 const router = express.Router()
 
 router.post("/create", createShortUrl)
-router.get("/:id", redirectFromShortUrl)
 
 export default router;
