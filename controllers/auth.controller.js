095b5c063c4a5a826b48caa1a18a26a85423580f
rahull0328 <rmmehta2004@gmail.com>
2025-08-29 22:04:42 +0530

    added authService along with skeleton for auth controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import wrapAsync from "../utils/tryCatchWrapper"
+
+export const register = wrapAsync(async(req, res) => {
+
+})
+
+export const login = wrapAsync(async(req, res) => {
+
+})
+
+export const logout = wrapAsync(async(req, res) => {
+
+})
+
+export const getCurrentUser = wrapAsync(async(req, res) => {
+
+})
