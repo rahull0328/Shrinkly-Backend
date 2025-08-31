93cf97a3cff8d1d18e81eb199f3bacdd7ec0a73c
rahull0328 <rmmehta2004@gmail.com>
2025-08-31 21:10:03 +0530

    added login and middleware functionality

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,12 @@
 import express from "express"
 import { register, login, logout, getCurrentUser } from "../controllers/auth.controller.js"
-// import { authMiddleware } from "../middleware/auth.middleware.js"
+import { authMiddleware } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
 router.post("/register", register)
 router.post("/login", login)
 router.post("/logout", logout)
-router.get("/me", getCurrentUser)
+router.get("/profile", getCurrentUser)
 
 export default router
